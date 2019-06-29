4412e372ea1a2ac74009b1070bc4a5a2eab7baa8
ann <976964655@qq.com>
2019-06-29 15:22:28 +0800

    change object

---
+++ q1.js
@@ -0,0 +1,11 @@
+var user={}
+
+user.name="John"
+
+user.surname="Mike"
+
+user.name="Peter"
+
+delete  user.name
+
+
---
+++ q2.js
@@ -0,0 +1,13 @@
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+// write your code here...
+var num=0
+for(i in fruit){
+    num+=fruit[i]
+}
+console.log(num)
+
+
